5cd5af68f2693f1df1b3b7341ed15e340cd3e321
GurikaK77 <mnxmidnight777@gmail.com>
2026-03-19 18:16:27 +0900

    fixes

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -19,6 +19,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Restraunt-Management-System');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Restaurant-Management-System');
   });
 });
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import { SummaryInfo } from "../components/summary-info/summary-info";
+import { TotalCustomers } from "../components/total-customers/total-customers";
+import { TodayReservations } from '../components/today-reservations/today-reservations';
+import { TotalMenus } from '../components/total-menus/total-menus';
+import { TotalRestaurants } from '../components/total-restaurants/total-restaurants';
+import { ReservationList } from '../components/reservation-list/reservation-list';
+import { ReservationStatus } from '../components/reservation-status/reservation-status';
+import { AllMenu } from '../components/all-menu/all-menu';
+import { AvailableDishes } from '../components/available-dishes/available-dishes';
+import { DishDetails } from '../components/dish-details/dish-details';
+import { Customers } from '../components/customers/customers';
+import { UserProfile } from '../components/user-profile/user-profile';
+import { RestaurantDetails } from '../components/restaurant-details/restaurant-details';
+import { CapacitySeats } from '../components/capacity-seats/capacity-seats';
+
+
+
+
+
+
+
+
+
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [SummaryInfo, TotalCustomers, TodayReservations, TotalMenus, TotalRestaurants, ReservationList, ReservationStatus, AllMenu, AvailableDishes, DishDetails, Customers, UserProfile, RestaurantDetails, CapacitySeats],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
