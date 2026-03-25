6e2900e0aaccdf2edca416e3911df5c57c52f290
GurikaK77 <mnxmidnight777@gmail.com>
2026-03-25 19:04:14 +0900

    auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Signup } from './pages/authorization/signup/signup';
+import { Login } from './pages/authorization/login/login';
 
 
 export const routes: Routes = [
     {path: "", component: Home},
     {path: "main", component: Home},
+    {path: "sign-up", component: Signup},
+    {path: "sign-in", component: Login},
+    
 ];
